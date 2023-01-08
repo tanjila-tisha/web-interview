78d46eabedde87af224ee0f3f0cdf66c381e3374
Tanjila Akhtar <37596431+tanjila-tisha@users.noreply.github.com>
2023-01-08 19:36:13 +0100

    Create todoServices.js

---
+++ frontend/src/data/todoServices.js
@@ -0,0 +1,30 @@
+import { BASE_URL } from "../constant/backend"
+
+// API services
+
+// Get all todos with existing list
+export const fetchTodoLists = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/todos`)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log('Unable to fetch data', error)
+  }
+}
+
+// Send to backend to save todos with list
+export const saveTodoList = async (data) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  }
+  try {
+    const response = await fetch(`${BASE_URL}/todos`, requestOptions)
+    const status = response.status
+    return status
+  } catch (error) {
+    console.log('Unable to save data', error)
+  }
+}
