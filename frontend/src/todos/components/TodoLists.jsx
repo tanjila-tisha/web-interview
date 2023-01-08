0f276c46590fd8a44a4ef5b12ddff72464277bb0
Tanjila Akhtar <37596431+tanjila-tisha@users.noreply.github.com>
2023-01-08 17:23:54 +0100

    Update TodoLists.jsx

--- frontend/src/todos/components/TodoLists.jsx
+++ frontend/src/todos/components/TodoLists.jsx
@@ -8,71 +8,22 @@ import {
   ListItemIcon,
   Typography,
 } from '@mui/material'
-import ReceiptIcon from '@mui/icons-material/Receipt'
 import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck'
 import ReorderIcon from '@mui/icons-material/Reorder'
 import { TodoListForm } from './TodoListForm'
 import Tooltip from '@mui/material/Tooltip'
-
-const fetchTodoLists = async () => {
-  try {
-    const response = await fetch('http://localhost:3001/todos')
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.log('Unable to fetch data', error)
-  }
-
-  /*return sleep(1000).then(() =>
-    Promise.resolve({
-      '0000000001': {
-        id: '0000000001',
-        title: 'First List',
-        todos: [
-          {
-            task: 'First todo of first list!',
-            completed: false,
-          },
-        ],
-      },
-      '0000000002': {
-        id: '0000000002',
-        title: 'Second List',
-        todos: [
-          {
-            task: 'First todo of second list!',
-            completed: false,
-          },
-        ],
-      },
-    })
-  )*/
-}
-
-const saveTodoList = async (data) => {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
-  }
-  try {
-    const response = await fetch('http://localhost:3001/todos', requestOptions)
-    const status = await response.json()
-    return status
-  } catch (error) {
-    console.log('Unable to save data', error)
-  }
-}
+import { fetchTodoLists, saveTodoList } from '../../data/todoServices'
+import { isTodoListCompleted } from '../../utils/todos'
 
 export const TodoLists = ({ style }) => {
   const [todoLists, setTodoLists] = useState({})
   const [activeList, setActiveList] = useState()
+  const [isButtonDisable, setIsButtonDisable] = useState(true)
+  const [confirmationMsg, setConfirmationMsg] = useState({})
 
   useEffect(() => {
     fetchTodoLists().then(setTodoLists)
   }, [])
-
-  const isTodoListCompleted = (todos) => !todos.filter((todo) => todo.completed === false).length
 
   const handleSave = (id, todos) => {
     const listToUpdate = todoLists[id]
@@ -80,8 +31,19 @@ export const TodoLists = ({ style }) => {
       ...todoLists,
       [id]: { ...listToUpdate, todos },
     }
-    console.log('updatedList:', updatedList)
-    saveTodoList(updatedList)
+
+    saveTodoList(updatedList).then((status) => {
+      if (status === 201) {
+        setIsButtonDisable(true)
+        setConfirmationMsg({ status: 'success', message: 'Todos has been saved successfully' })
+      } else {
+        setIsButtonDisable(false)
+        setConfirmationMsg({
+          status: 'error',
+          message: 'Unfortunately There is en error. Please try again.',
+        })
+      }
+    })
     setTodoLists(updatedList)
   }
 
@@ -93,22 +55,31 @@ export const TodoLists = ({ style }) => {
         <CardContent>
           <Typography component='h2'>My Todo Lists</Typography>
           <List>
-            {Object.keys(todoLists).map((key) => (
-              <ListItem key={key} button onClick={() => setActiveList(key)}>
-                <Tooltip
-                  title={isTodoListCompleted(todoLists[key].todos) ? 'Completed' : 'In progress'}
+            {Object.keys(todoLists).map((key) => {
+              const isCompleted =
+                todoLists[key].todos.length && isTodoListCompleted(todoLists[key].todos)
+              return (
+                <ListItem
+                  key={key}
+                  button
+                  onClick={() => {
+                    setActiveList(key)
+                    setConfirmationMsg({})
+                  }}
                 >
-                  <ListItemIcon>
-                    {isTodoListCompleted(todoLists[key].todos) ? (
-                      <PlaylistAddCheckIcon fontSize='large' color='primary' />
-                    ) : (
-                      <ReorderIcon />
-                    )}
-                  </ListItemIcon>
-                </Tooltip>
-                <ListItemText primary={todoLists[key].title} />
-              </ListItem>
-            ))}
+                  <Tooltip title={isCompleted ? 'Completed' : 'In progress'}>
+                    <ListItemIcon>
+                      {isCompleted ? (
+                        <PlaylistAddCheckIcon fontSize='large' color='primary' />
+                      ) : (
+                        <ReorderIcon />
+                      )}
+                    </ListItemIcon>
+                  </Tooltip>
+                  <ListItemText primary={todoLists[key].title} />
+                </ListItem>
+              )
+            })}
           </List>
         </CardContent>
       </Card>
@@ -117,6 +88,10 @@ export const TodoLists = ({ style }) => {
           key={activeList} // use key to make React recreate component to reset internal state
           todoList={todoLists[activeList]}
           saveTodoList={handleSave}
+          isButtonDisable={isButtonDisable}
+          setIsButtonDisable={setIsButtonDisable}
+          confirmationMsg={confirmationMsg}
+          setConfirmationMsg={setConfirmationMsg}
         />
       )}
     </Fragment>
