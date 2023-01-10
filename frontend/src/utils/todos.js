b9d9a90f05a2027c02ea4aa640edd64e1ff357d7
Tanjila Akhtar <37596431+tanjila-tisha@users.noreply.github.com>
2023-01-10 21:52:50 +0100

    Update todos.js

--- frontend/src/utils/todos.js
+++ frontend/src/utils/todos.js
@@ -1,1 +1,15 @@
+// Check if all todos are completed in the todo list
 export const isTodoListCompleted = (todos) => !todos.filter((todo) => todo.completed === false).length
+
+// Calculate the remaning days left for the todo
+export const calculateRemaningDays = (dueDate) => {
+    const currentDate = new Date().getTime()
+    const dateDiff = new Date(dueDate).getTime() - currentDate
+    let remaningDays = Math.abs(dateDiff) / (1000 * 3600 * 24)
+    
+    if(dateDiff < 0){
+        remaningDays = -remaningDays;
+    }
+   
+    return Math.ceil(remaningDays)
+  }
